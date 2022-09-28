a39d1975ba9439b9e54a6a4ef46f67da52848bb4
MaxatDzhetenbayev <yaboku18@mail.ru>
2022-09-28 09:57:26 +0600

    updated genres features

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -9,13 +9,15 @@ const MovieCard = ({ genres, title, description, year, image }) => {
       <div className='movie-card'>
          <img className='movie-card__img' src={image} alt="" />
          <div className='movie-card__body'>
-            <div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.6rem' }}>
-               {genres.map((genre) => (
+            <div style={{ display: 'flex',  gap: '0.4rem',  fontSize: '14px' }}>
+               {genres.slice(0, 2).map((genre) => (
                   <ItemTag key={genre} title={genre} />
                ))}
+               {genres.length > 2 && <ItemTag title={`Еще +${genres.length - 2} `}/>}
+               
             </div>
             <div className='movie-card__title'>{title} ({year})</div>
-            <div className='movie-card__text' >{description.slice(0, 65)}...</div>
+            <div className='movie-card__text' >{description.slice(0, 80)}...</div>
          </div>
 
       </div>
